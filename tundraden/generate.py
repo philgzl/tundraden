9f430b455948af4ed568ceeb80114b30d36fa6e4
unknown <gonzalez.philippe@outlook.com>
2019-11-21 14:12:15 +0100

    Some module and function renaming

--- tundraden/generate.py
+++ tundraden/generate.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 
-def generate_correlated_data(covariance_matrix, n_samples):
+def regressors(covariance_matrix, n_samples):
     n_regressors = covariance_matrix.shape[0]
     mu = np.zeros(n_regressors)
     X = np.random.multivariate_normal(mu, covariance_matrix, size=n_samples)
     return X
 
 
-def generate_target(regressors, weights, sigma):
+def target(regressors, weights, sigma):
     noise = np.random.normal(scale=sigma, size=len(regressors))
     return np.dot(regressors, weights) + noise
